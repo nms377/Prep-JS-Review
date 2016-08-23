306f74af393d978efe748b3d19571155172e929f
nms377 <nms377@nyu.edu>
2016-08-22 22:02:21 -1000

    left off on No. 19. I forgot how to do this one...

--- js/app.js
+++ js/app.js
@@ -8,15 +8,36 @@ Go get em!*/
 /*1.  Variables with a String as their value.
 Declare variables named yourName, neighborName, teacherName.  Console log each variable.*/
 
+var yourName = 'Coco';
+var neighborName = 'Randy';
+var teacherName = 'Vic';
+
+console.log(yourName, neighborName, teacherName);
 
 /*2. Variable with a Number as their value.
 Declare variables named numberOfFloorsBurjKhalifa, TokyoOlympicsYear.  Console log each variable.*/
 
+var numberOfFloorsBurjKhalifa = 163;
+var TokyoOlympicsYear = 2020;
+
+console.log(numberOfFloorsBurjKhalifa, TokyoOlympicsYear);
+
 /*3. Variables with Boolean values
 Declare variables named lovesPrepClass, eatsDonuts, drinksGin.  Console log each variable.*/
 
+var lovesPrepClass = true;
+var eatsDonuts = true;
+var drinksGin = false;
+
+console.log(lovesPrepClass, eatsDonuts, drinksGin);
+
 /*4. Variable with Null values
 Declare variables named completedCodingChallenge, traveledToSpace.  Console log each variable*/
+
+var completedCodingChallenge = null;
+var traveledToSpace = null;
+
+console.log(completedCodingChallenge, traveledToSpace);
 
 /*5.  Variables with Array for its value.
 Declare a variable named japanPrefectures and assign the value of the names of 5 prefectures in Japan.
@@ -24,6 +45,10 @@ Console log the variable
 Console log the value at index 1.
 Console log the value at index 3.*/
 
+var japanPrefectures = ['Osaka', 'Kyoto','Hokkaido','Tokyo','Fukui'];
+console.log(japanPrefectures);
+console.log(japanPrefectures[1]);
+console.log(japanPrefectures[3]);
 
 /*6.  Variable with Array for its value.
 Declare a variable named planets and assign the value of each planet in our solar system, in the order of distance from the sun.  
@@ -31,9 +56,18 @@ Console log the variable
 Console log "Earth" using the index position
 Console log "Uranus" using the index position*/
 
+var planets = ['mercury', 'venus', 'earth', 'mars', 'jupiter', 'uranus', 'neptune', 'pluto'];
+
+console.log(planets);
+console.log(planets[2]);
+console.log(planets[5]);
+
 /*7. Variables with Array for its value.
 Declare a variable named india and assign the value of 3 popular tourist attractions.  
 Console log the variable.*/
+
+var india = ['taj mahal', 'ladakh', 'ganges'];
+console.log(india);
 
 /*8.  Declare a literal object named "hotel" with properties and values set to:
 
@@ -44,6 +78,15 @@ Console log the variable.*/
 
  console log the object*/
 
+ var hotel = {
+  name: 'Hilton',
+  rooms: 200,
+  pool: true,
+  checkIn: null
+};
+
+console.log(hotel);
+
 /*9.  Declare a literal object named "car" with properties and values set to:
 
     make  => "BMW"
@@ -53,6 +96,15 @@ Console log the variable.*/
 
  console log the object*/
 
+ var car = {
+  make: 'BMW',
+  model: 'X5',
+  year: 2015,
+  doors: 4
+ };
+
+ console.log(car);
+
 /*10.  Declare a literal object named bigBox and set its value to be an empty object.
 
 Add a property named size and set it to a random number value between 0 and 50 (hint, use Math.something...)
@@ -61,6 +113,13 @@ Add a property named contents and set it to be an empty array
 
 console log the object*/
 
+var bigBox = {};
+
+bigBox.size = Math.floor(Math.random()*50);
+bigBox.color = 'purple';
+bigBox.contents = [];
+
+console.log(bigBox);
 
 /*11.  Declare a literal object named "colorMeBadd" with properties and values set to:
 
@@ -77,30 +136,86 @@ console log the object*/
 
 console log the object */ 
 
+var colorMeBadd = {
+
+iro: {
+    akai: 'red',
+    aoi: 'blue',
+    midori: 'green',
+    shiroi: 'white',
+  },
+isJapanese: true
+};
+
+console.log(colorMeBadd);
+
 /*For exercises #12 - 14, declare two variables, one named "dog" and the other named "cat".  Assign a number value for each variable.*/
+
+var dog = 5;
+var cat = 2;
 
 /*12.  Declare a function named add.
 This function will take two parameters and returns the result of adding two numbers together.
 Store this value in a variable named sum and console log this variable.*/
 
+function add(n1, n2){
+  return n1 + n2;
+}
+
+console.log(add(dog, cat));
+
 /*13.  Declare a function named subtract. 
 This function will take two parameters and returns the result of subtracting two numbers together. 
 Store this value in a variable named difference and console log this variable.*/
+
+function subtract(n1, n2){
+  return n1-n2;
+}
+
+console.log(subtract(dog, cat));
 
 /*14. Declare a function named multiply.
 This function will take two parameter and return the result of multipling two numbers together.
 Store this value in a variable named product and console log this variable.*/
 
+function multiply(n1, n2){
+  return n1*n2;
+}
+
+console.log(multiply(dog, cat));
+
 /*15.  Declare a function named dunkinDonuts.
 This function will access the value stored in the sum variable and uses this number to return the string "I eats X old fashioned donuts every morning to stay classy." X is the value stored in the sum variable.  Console log the result.  */ 
 
+function dunkinDonuts(n1, n2){
+  var X = add(n1, n2);
+  return 'I eats ' +X+' old fashioned donuts every morning to stay classy';
+}
+
+console.log(dunkinDonuts(dog, cat));
+
 /*16.  Declare a function named stayinClassy.
 This function will access the values store in the difference and product variables and uses these numbers to return the string "Yea, I drank X gallons of box wine and ate Y bags of Cheetos and still feel vibrant yo!"  X is the value stored at difference variable and Y is the value stored at product variable. Console.log the result.*/
+
+function stayinClassy(n1, n2){
+  var x = subtract(n1, n2);
+  var y = multiply(n1, n2);
+  return 'Yea, I drank ' + x + ' gallons of box wine and ate ' + y + ' bags of Cheetos and still feel vibrant yo!';
+}
+
+console.log(stayinClassy(dog, cat));
 
 /*17.  Declare a function named watMyName, which takes two parameters firstName and lastName.
 This function will return a string "My name is...firstName lastName."
 Invoke this function by passing in the string values of "Slim", "Shady". and console log the result.
 */
+
+function watMyName(firstName, lastName){
+  return 'My name is...' + firstName + ' ' + lastName + '.';
+}
+
+console.log(watMyName('Slim', 'Shady'));
+
 
 /*18. Using the japanPrefectures array you created in exercise 5, create a for loop that iterates through this array and console logs the following message:
 
@@ -108,8 +223,18 @@ Invoke this function by passing in the string values of "Slim", "Shady". and con
 
 i.e. "The locaton at 0 is Tokyo."*/
 
+for (var i=0; i<japanPrefectures.length; i++){
+  console.log(japanPrefectures[i]);
+  console.log('The location at ' + i + ' is: ' + japanPrefectures[i] + '.');
+}
+
 /*19. Declare a variable named "oddNumbers" and assign it to an empty array
 Use a for loop to add only odd numbers to an Array.  Add 30 odd numbers to the oddNumbers variable starting with the value of 1.*/
+
+var oddNumbers = [];
+
+for(var i=0; i<30;i--){
+}
 
 /*20.  Declare a function named "oldEnough".
 This function takes in a number parameter age and returns a Boolean value true or false if the age is lower than the minimum legal drinking age in Antigua.
